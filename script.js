21f527a17eae415877322426c30e74545e1de07d
Chasen Jeffries <jeffriesc15@gmail.com>
2024-11-28 14:00:25 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 (function ($) {
   $(window).on('load hashchange', function(){
-    $('.content-region').hide();  // Hide all content regions
-    var region = location.hash.toString() || '#Home';  // Get the hash or default to '#Home'
-    $(region).show();  // Show the region that matches the hash
+    // If the hash matches specific IDs, show/hide regions without preventing default behavior
+    if (location.hash) {
+      $('.content-region').hide();  // Hide all content regions
+      var region = location.hash.toString();  // Get the hash
+      $(region).show();  // Show the region that matches the hash
+    }
   });
 })(jQuery);
